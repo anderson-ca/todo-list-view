04efb3dfcb0fc9e6dd9610848a30555744c53db1
Anderson Cardoso <anderson.cardoso@thesslstore.com>
2023-02-10 15:51:41 -0600

    make it work

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: "ae0c8525c1d4d3ff135b",
-      clientSecret: "bac98f6251169a8f314829f4ecd2170aa88c973c",
+      clientId: "13c2bfdedb1958dd3468",
+      clientSecret: "d90cc5764e66325b61c2259114244308b399686c",
     }),
     // ...add more providers here
   ],
